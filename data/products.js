d6ec528ecbc502f4c087075cb49d7ee6c943d568
pablolindo69 <pablojulian326@gmail.com>
2025-06-16 18:05:20 -0500

    Proyecto backend

---
+++ data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Nike Air Max 90',
+    image: '/images/airmax90.png',
+    description:
+      'Clásico diseño con unidad Air visible en el talón, malla y ante para mayor comodidad y soporte, suela waffle para tracción duradera.',
+    brand: 'Nike',
+    category: 'Zapatillas',
+    price: 129.999,
+    countInStock: 15,
+    rating: 4.7,
+    numReviews: 34,
+  },
+  {
+    name: 'Adidas Ultraboost 22',
+    image: '/images/ultraboost22.png',
+    description:
+      'Entresuela Boost de máximo retorno de energía, upper Primeknit adaptativo y suela Stretchweb con caucho Continental para excelente tracción.',
+    brand: 'Adidas',
+    category: 'Zapatillas',
+    price: 179.999,
+    countInStock: 9,
+    rating: 4.8,
+    numReviews: 27,
+  },
+  {
+    name: 'Puma RS-X³',
+    image: '/images/rsx3.png',
+    description:
+      'Diseño retro-futurista con amortiguación ProFoam de gran reactividad, capa de TPU y suela de goma resistente al desgaste.',
+    brand: 'Puma',
+    category: 'Zapatillas',
+    price: 109.990,
+    countInStock: 12,
+    rating: 4.3,
+    numReviews: 18,
+  },
+  {
+    name: 'New Balance 574',
+    image: '/images/nb574.png',
+    description:
+      'Ícono atemporal con entresuela ENCAP para soporte y durabilidad, upper de ante y malla transpirable, perfecta para uso diario.',
+    brand: 'New Balance',
+    category: 'Zapatillas',
+    price: 189.999,
+    countInStock: 20,
+    rating: 4.5,
+    numReviews: 42,
+  },
+  {
+    name: 'Reebok Classic Leather',
+    image: '/images/reebok_classic.png',
+    description:
+      'Clásico de los 80 con upper de piel suave, entresuela de EVA acolchada y suela de goma de perfil bajo para confort ligero.',
+    brand: 'Reebok',
+    category: 'Zapatillas',
+    price: 174.990,
+    countInStock: 5,
+    rating: 4.2,
+    numReviews: 15,
+  },
+  {
+    name: 'Converse Chuck Taylor All Star',
+    image: '/images/chuck_taylor.png',
+    description:
+      'Zapatilla legendaria con capellada de lona resistente, puntera de goma y suela vulcanizada para un estilo auténtico.',
+    brand: 'Converse',
+    category: 'Zapatillas',
+    price: 159.990,
+    countInStock: 25,
+    rating: 4.6,
+    numReviews: 60,
+  },
+];
+
+export default products;
